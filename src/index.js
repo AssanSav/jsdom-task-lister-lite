e1f31ee0f1f25b175f4b67444f9fc4ec4c12b6ec
AssanSav <assane.savadogo81@gmail.com>
2020-02-08 11:56:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addTaskToDoWithDeleteOption()
 });
+
+function addTaskToDoWithDeleteOption() {
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+    const userInput = event.target.querySelector("#new-task-description").value;
+    const showTasks = document.getElementById("tasks");
+    const li = document.createElement("li");
+    showTasks.appendChild(li);
+    li.textContent = userInput;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "x";
+    li.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", () => {
+      li.remove();
+    })
+  })
+}
